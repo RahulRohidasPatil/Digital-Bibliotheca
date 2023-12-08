138b0df068da02542eadff85e7f8cda84f106359
Rahul Patil <rahul-rohidas.patil@informatik.hs-fulda.de>
2023-12-08 21:37:54 +0100

    Product info page updated with other details of media

--- react-app/src/pages/product-info-page.jsx
+++ react-app/src/pages/product-info-page.jsx
@@ -1,22 +1,87 @@
-import { Container, Typography } from "@mui/material";
+import { Box, Button, Card, Container, Grid, Rating, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getMedia } from "src/apis/media";
 
 export default function ProductInfoPage() {
     const { id } = useParams();
-    const [productDetails, setProductDetails] = useState();
+    const [product, setproduct] = useState();
 
     useEffect(() => {
         getMedia(id)
-            .then(response => setProductDetails(response.data.data));
+            .then(response => setproduct(response.data.data));
     }, [id]);
 
     return <Container>
-        <Typography variant="h4" sx={{ mb: 5 }}>
-            {productDetails?.Title}
-        </Typography>
+        <Grid container spacing={2}>
+            <Grid item xs={6}>
+                <Typography variant="h4" sx={{ mb: 5 }}>
+                    {product?.Title}
+                </Typography>
+                <Card sx={{ width: 300, height: 300 }}>
+                    <Box sx={{ pt: '100%', position: 'relative' }}>
+                        {product?.status}
 
-
+                        <Box
+                            component="img"
+                            alt={product?.name}
+                            src={product?.FilePath}
+                            sx={{
+                                top: 0,
+                                width: 1,
+                                height: 1,
+                                objectFit: 'cover',
+                                position: 'absolute',
+                            }}
+                        />
+                    </Box>
+                </Card>
+                <Typography sx={{ marginTop: 2 }} variant="caption" component="div">
+                    Description: {product?.Description}
+                </Typography>
+                <Typography sx={{ marginTop: 2 }} variant="caption" component="div">
+                    Pricing: {product?.Price}
+                </Typography>
+                <Typography sx={{ marginTop: 2 }} variant="caption" component="div">
+                    Content Type: {product?.MediaType}
+                </Typography>
+                <Typography sx={{ marginTop: 2 }} variant="caption" component="div">
+                    Released on: {product?.CreatedDate.split('T')[0]}
+                </Typography>
+            </Grid>
+            <Grid item xs={6}>
+                <Typography sx={{ marginTop: 2, fontSize: 20 }} variant="caption" component="div">
+                    Reviews:
+                </Typography>
+                <Rating value={5} readOnly />
+                <Box
+                    sx={{
+                        width: '400px', // Adjust width as needed
+                        height: '150px', // Adjust height as needed
+                        borderRadius: '10px', // Adjust border-radius for rounded corners
+                        backgroundColor: '#f0f0f0', // Placeholder color
+                        marginTop: '10px', // Adjust margin as needed
+                    }}
+                />
+                <Box
+                    sx={{
+                        width: '400px', // Adjust width as needed
+                        height: '150px', // Adjust height as needed
+                        borderRadius: '10px', // Adjust border-radius for rounded corners
+                        backgroundColor: '#f0f0f0', // Placeholder color
+                        marginTop: '10px', // Adjust margin as needed
+                    }}
+                />
+                <Button
+                    sx={{ marginTop: 1 }}
+                    variant="outlined"
+                    onClick={() => {
+                        console.log('Initiate chat with creator');
+                    }}
+                >
+                    Chat with Creator
+                </Button>
+            </Grid>
+        </Grid>
     </Container>
 }
